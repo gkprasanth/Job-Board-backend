d6dcd7612ff93876da327c4efcb0ffd35fe90b07
gkprasanth <gkpc2004@gmail.com>
2024-11-08 10:42:14 +0530

    first commit

---
+++ routes/adminRouters.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { addJob, getallJobs, getAppliedJobs, deleteJob,getNewApplications ,getActiveJobs, orderedJobs,acceptApplication, rejectApplication, changeStatus, addComment, getJobApplication, getUserDetailsByJobId, sendEmailToUser,sheduledDateTime,disableUser} = require('../controllers/adminController');
+
+const router = express.Router();
+
+// Define routes for adding a job and retrieving all jobs
+router.post('/add-job', addJob);
+router.get('/all-jobs', getallJobs);
+router.get('/applyed', getAppliedJobs);
+router.delete('/delete-job/:id', deleteJob);
+// router.get('/rejected', getallJobs);
+router.get('/newApplications',getNewApplications);
+router.get('/activeJobs',getActiveJobs);
+router.get('/orderedJobs/:id',orderedJobs);
+router.put('/accept-application/:id', acceptApplication);
+router.put('/reject-application/:id', rejectApplication);
+router.put('/change-status/:id', changeStatus);
+router.post('/add-comment/:id', addComment);
+router.get('/get-job-application/:id', getJobApplication);
+router.get('/get-user-details/:id', getUserDetailsByJobId);
+router.post('/send-email', sendEmailToUser);
+router.post('/sheduled-date-time/:id', sheduledDateTime);
+router.get('/disable-user/:id', disableUser);
+
+module.exports = router;
---
+++ routes/userRouters.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { getProfile, updateProfile , getallJobs, getJobById, applyJob, getJobByUser, getEducationDetails, getJobBySkills} = require('../controllers/userController');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Welcome to the User Page');
+});
+
+router.get('/profile', getProfile);
+router.put('/update-profile', updateProfile);
+router.get('/get-all-jobs', getallJobs); //only active jobs registration validaty >= today
+router.get('/get-by-id/:id', getJobById);
+router.post('/apply-job', applyJob);
+router.get('/get-applied-jobs', getJobByUser);
+router.get('/get-personal-details', getEducationDetails);
+router.get('/get-job-by-skills', getJobBySkills);
+module.exports = router;
